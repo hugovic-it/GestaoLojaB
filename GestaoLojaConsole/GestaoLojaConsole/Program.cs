57050274a4c8d6ffee1bf8065925ebf6c1eb25be
Hugo Victor <hugovic.it@gmail.com>
2024-08-08 12:09:54 -0300

    Projeto inicial

---
+++ GestaoLojaConsole/GestaoLojaConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestaoLojaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olár, mundo!");
+        }
+    }
+}
