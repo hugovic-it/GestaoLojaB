bc279650e3a9a2ca4ff0de9157b225952e250194
Hugo Victor <hugovic.it@gmail.com>
2024-08-09 05:33:26 -0300

    context e models

--- GestaoLojaConsole/GestaoLojaConsole/Context/AppDbContext.cs
+++ GestaoLojaConsole/GestaoLojaConsole/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GestaoLojaConsole.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace GestaoLojaConsole.Context
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        
     }
 }
--- GestaoLojaConsole/GestaoLojaConsole/Models/Produto.cs
+++ GestaoLojaConsole/GestaoLojaConsole/Models/Produto.cs
@@ -17,11 +17,13 @@ public class Produto
     public string? Nome { get; set; }
     [StringLength(10)]
     public string? Genero { get; set; }
-    [Range(1, 16)] //16 anos ? 
+    [Range(1, 16)] //16 anos ?  //Tamanho
     public int Idade { get; set; }
     [Range(0.00, 999.99)]
     public decimal ValorAtacado { get; set; }
     [Range(0.00, 999.99)]
     public decimal ValorFinal { get; set; }
+    [StringLength(10)]
+    public string? Status { get; set; }  //disponivel, vendido...
 
 }
